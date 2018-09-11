c66588408a342a5c5d98d304ac5a8047104bc729
wntitus <wesn.titus@gmail.com>
2018-09-11 12:01:19 -0400

    adding hbs layout and models folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -16,10 +17,13 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 mongoose.connect('mongodb://localhost/web-scraper', { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
-
+    res.render('index');
 });
 
 app.listen(PORT, () => {
